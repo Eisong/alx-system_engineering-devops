b62a0831a0c6cf2296b4f54322497c015e53bc45
Edet Isong <eddieisong1@gmail.com>
2023-11-24 13:21:49 -0800

    DevOps Project on 0x0A. Configuration management by Edet Isong

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 # Install Flask version 2.1.0 using pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin'],
+  creates => '/usr/local/lib/python3.x/dist-packages/flask',
 }
