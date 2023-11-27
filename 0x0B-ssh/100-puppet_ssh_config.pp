75c0ab0b8411e94a7cf0ba557a17e2aa46b2a194
Edet Isong <eddieisong1@gmail.com>
2023-11-27 06:28:27 -0800

    SSH Project on 0x0B. SSH by Edet Isong

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,15 +2,15 @@
 #using puppet to connect without password
 
 file{'/etc/ssh/ssh_config':
- ensure => present,
+	ensure => present,
 }
 file_line{'Turn off passwd auth':
- path => '/etc/ssh/ssh_config',
- line => 'PasswordAuthencation no',
- match => '^#PasswordAuthentication',
+	path => '/etc/ssh/ssh_config',
+	line => 'PasswordAuthencation no',
+	match => '^#PasswordAuthentication',
 }
 file_line{'Declare Identity filee':
- path => '/etc/ssh/ssh_config',
- line => 'IdentityFile ~/.ssh/school',
- match => '^#IdentityFile',
+	path => '/etc/ssh/ssh_config',
+	line => 'IdentityFile ~/.ssh/school',
+	match => '^#IdentityFile',
 }
