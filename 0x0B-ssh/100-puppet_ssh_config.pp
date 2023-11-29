fb891763cba6c144a8a3f55ee0d842815672afe1
Edet Isong <eddieisong1@gmail.com>
2023-11-28 16:10:06 -0800

    SSH Project on 0x0B. SSH by Edet Isong

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-#using puppet to connect without password
 
+#using puppet to connect without password
 file_line{'Turning off Password based authentication':
-	ensure  => 'present',
-	path    => '/etc/ssh/ssh_config',
-	line    => 'PasswordAuthentication',
-	replace => true
+ ensure  => 'present',
+ path    => '/etc/ssh/ssh_config',
+ line    => 'PasswordAuthentication',
+ replace => true
 }
